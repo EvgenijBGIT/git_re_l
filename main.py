02606246ac2d459b1c1de38f9fe15309b9d71ec4
Evgenij <fffuuufff0@mail.ru>
2024-02-16 09:20:38 +0300

    main.py to master branch

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('And now it is on GIT')
     print('Is it?')
+    print('And now it is in ???master??? branch')
 
 
 if __name__ == '__main__':
